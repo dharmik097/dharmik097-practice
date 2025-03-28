b3cdf24a7ef49d66138e02f5e61db46854009181
dharmik097 <dvs97.cs@gmail.com>
2025-03-28 12:18:49 +0000

    just prac

---
+++ code.py
@@ -0,0 +1,46 @@
+# print ("shri ganeshay namah")
+
+# print ( "jay Hind")
+
+# a = 25
+# b = 5
+
+# print (a + b)
+# print (a - b)
+# print (a ** b)
+# print (a % b)
+
+
+# name = input("enter name: ")
+# age = input("enter age: ")
+# marks = input("enter marks: ")
+
+# print("welcome", name)
+# print("age =", age)
+# print("marks =", marks) dhara
+
+# a = float( input ("first value: "))
+# b = float( input ("second value: "))
+# c = ( a + b )
+# print ( "sum of the both values =", c )
+
+# Input two numbers from the user
+# num1 = float(input("Enter the first number: "))
+# num2 = float(input("Enter the second number: "))
+
+# # Calculate the sum
+# sum = num1 + num2
+
+# # Print the sum
+# print("The sum of", num1, "and", num2, "is:", sum)
+
+# side = float(input("enter square side : " ))
+# print("aria =", side * side )
+
+# value1 = float(input("enter first value: " ))
+# value2 = float(input("enter second value: " ))
+# print("average =", (value1 + value2 )/2)
+
+# a = int(input("enter first value: " ))
+# b = int(input("enter second value: " ))
+# print (a >= b)
