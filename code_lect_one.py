134891451fe284887a6043cf809feb789d4006f5
dharmik097 <dvs97.cs@gmail.com>
2025-04-14 20:04:45 +0100

    mods

--- code_lect_one.py
+++ code_lect_one.py
@@ -1,45 +1,48 @@
-# print ("shri ganeshay namah")
+print ("shri ganeshay namah")
 
-# print ( "jay Hind")
+print ( "jay Hind")
 
-# a = 25
-# b = 5
+a = 25
+b = 5
 
-# print (a + b)
-# print (a - b)
-# print (a ** b)
-# print (a % b)
+print (a + b)
+print (a - b)
+print (a ** b)
+print (a % b)
 
 
-# name = input("enter name: ")
-# age = input("enter age: ")
-# marks = input("enter marks: ")
+name = input("enter name: ")
+age = input("enter age: ")
+marks = input("enter marks: ")
 
-# print("welcome", name)
-# print("age =", age)
-# print("marks =", marks) dhara
+print("welcome", name)
+print("age =", age)
+print("marks =", marks)
 
-# a = float( input ("first value: "))
-# b = float( input ("second value: "))
-# c = ( a + b )
-# print ( "sum of the both values =", c )
 
-# Input two numbers from the user
-# num1 = float(input("Enter the first number: "))
-# num2 = float(input("Enter the second number: "))
 
-# # Calculate the sum
-# sum = num1 + num2
 
-# # Print the sum
-# print("The sum of", num1, "and", num2, "is:", sum)
+a = float( input ("first value: "))
+b = float( input ("second value: "))
+c = ( a + b )
+print ( "sum of the both values =", c )
 
-# side = float(input("enter square side : " ))
-# print("aria =", side * side )
+Input two numbers from the user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
 
-# value1 = float(input("enter first value: " ))
-# value2 = float(input("enter second value: " ))
-# print("average =", (value1 + value2 )/2)
+# Calculate the sum
+sum = num1 + num2
+
+# Print the sum
+print("The sum of", num1, "and", num2, "is:", sum)
+
+side = float(input("enter square side : " ))
+print("aria =", side * side )
+
+value1 = float(input("enter first value: " ))
+value2 = float(input("enter second value: " ))
+print("average =", (value1 + value2 )/2)
 
 a = int(input("enter first value: " ))
 b = int(input("enter second value: " ))
