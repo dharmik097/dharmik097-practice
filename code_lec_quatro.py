52c80f85d77dc58bf4d0ba8e2ba95bde665e6391
dharmik097 <dvs97.cs@gmail.com>
2025-04-14 18:08:08 +0100

    mods

---
+++ code_lec_quatro.py
@@ -0,0 +1,11 @@
+info = {
+    
+    "name" : "stallwarts",
+"subjects" : ["Python","C","Java","C#","R",]
+    "learning" : "coding"
+    "age" : 27,
+    "is_adult" : True,
+    "marks" : 95.5
+
+
+}
