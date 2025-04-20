63a788979732e1130273ace5e556bc2ac3db8f81
dharmik097 <dvs97.cs@gmail.com>
2025-04-21 00:38:50 +0100

    adds

--- Python-practice-level-1/code_lec_cinco.py
+++ Python-practice-level-1/code_lec_cinco.py
@@ -12,4 +12,9 @@ i = 1
 while i <= 5:
     print(i)
     i += 1
-    
+
+#reverse
+i = 5
+while i >= 1:
+    print(i)
+    i -= 1
--- Python-practice-level-1/tempCodeRunnerFile.py
+++ Python-practice-level-1/tempCodeRunnerFile.py
@@ -1,5 +1,5 @@
-i = 1
-while i <= 5:
+
+i = 5
+while i >= 1:
     print(i)
-    i += 1
-    
+    i -= 1
