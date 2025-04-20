54925c38577ea33e6466c6f67b86920a1da4791d
dharmik097 <dvs97.cs@gmail.com>
2025-04-21 00:26:44 +0100

    adds

--- Python-practice-level-1/code_lec_cinco.py
+++ Python-practice-level-1/code_lec_cinco.py
@@ -1,7 +1,15 @@
 #loops
 #2 types while and for
  
-count = 1
+count = 9
 while count <= 10 :
     print("hello")
-    count += 1
+    count += 5
+
+
+#PRINT NUMBERS 1 TO 5
+i = 1
+while i <= 5:
+    print(i)
+    i += 1
+    
--- Python-practice-level-1/tempCodeRunnerFile.py
+++ Python-practice-level-1/tempCodeRunnerFile.py
@@ -1,4 +1,5 @@
-count = 1
-while count <= 5 :
-    print("hello")
-    count += 1
+i = 1
+while i <= 5:
+    print(i)
+    i += 1
+    
