35b36ef3b28a596b01b7f041772db7359b1f0016
dharmik097 <dvs97.cs@gmail.com>
2025-04-23 17:53:36 +0100

    serch num fmr tupple useing loop

--- Python-practice-level-1/tempCodeRunnerFile.py
+++ Python-practice-level-1/tempCodeRunnerFile.py
@@ -1,6 +1,10 @@
 
-heros = ["Dharam","Ironman","Thor"]
-idx = 0
-while idx < len(heros):
-    print(heros[idx])
-    idx += 1
+nums = (1,2,4,9,16,25,36,49,64,81,100,36,10,15,36,57,85,36)
+x =36
+i = 0
+while i < len(nums):
+    if(nums[i] == x):
+        print(x,"found at index",i)
+    else:
+        print("finding..")
+    i += 1    
