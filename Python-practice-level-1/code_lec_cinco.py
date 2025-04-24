3f7ed30f0ced0be0dc073cbf39ef5ca793ed0903
dharmik097 <dvs97.cs@gmail.com>
2025-04-24 23:36:13 +0100

    pracs

--- Python-practice-level-1/code_lec_cinco.py
+++ Python-practice-level-1/code_lec_cinco.py
@@ -271,5 +271,22 @@ for i in range(1,n+1):
     sum += i
 print("total sum =", sum)
 
+# wAP to find the factorial of first n numbers (using for)
+
+n = int(input("factorial of first n numbers where n ="))
+fact = 1
+i = 1
+while i <= n:
+    fact *= i 
+    i += 1
+
+print("factorial =", fact)
 
 
+n = int(input("factorial of first n numbers where n ="))
+fact = 1
+
+for i in range(1, n+1):
+    fact *= i
+
+print("facts =", fact)    
--- Python-practice-level-1/tempCodeRunnerFile.py
+++ Python-practice-level-1/tempCodeRunnerFile.py
@@ -1,5 +1,8 @@
-n = int(input("sum of first n numbers where n ="))
-sum = 0
-for i in range(1,n+1):
-    sum += i
-print("total sum =", sum)
+n = int(input("factorial of first n numbers where n ="))
+fact = 1
+i = 1
+while i <= n:
+    fact *= i 
+    i += 1
+
+print("factorial =", fact)
