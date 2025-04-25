14fe540f48eca4c6387188c08e40c94904968e3f
dharmik097 <dvs97.cs@gmail.com>
2025-04-25 13:52:12 +0100

    editing

--- Python-practice-level-1/code_lect_cinco.py
+++ Python-practice-level-1/code_lect_cinco.py
@@ -291,4 +291,3 @@ for i in range(1, n+1):
 
 print("facts =", fact)
 
-
--- Python-practice-level-1/tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-n = int(input("factorial of first n numbers where n ="))
-fact = 1
-i = 1
-while i <= n:
-    fact *= i 
-    i += 1
-
-print("factorial =", fact)
