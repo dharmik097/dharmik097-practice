8a045f72fa1368d678011c183010181b876d536c
dharmik097 <dvs97.cs@gmail.com>
2025-04-26 21:33:24 +0100

    updates

---
+++ Python-practice-level-1/code_lect_ses.py
@@ -0,0 +1,7 @@
+#Functions
+#Block of statement that performs a specific task
+#Function is a reusable piece of code that can be called when needed
+
+def calc_sum(a, b):
+    sum = a + b
+    print("The sum of a and b is :", sum)
