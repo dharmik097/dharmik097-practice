edf08edfdcbd314f1c54ec2547c127b8a76e0520
dharmik097 <dvs97.cs@gmail.com>
2025-04-27 23:06:08 +0100

    adds

--- Python-practice-level-1/tempCodeRunnerFile.py
+++ Python-practice-level-1/tempCodeRunnerFile.py
@@ -1,5 +1,5 @@
 f = open("README.md", "r") #open the file in read mode.
-data = f.read() #read the file.
-print(data) #print the file.
-print(type(data))
+line1 = f.readline() #read the file.
+print(line1) #print the file.
+print(type(line1))
 f.close() #close the file.  
