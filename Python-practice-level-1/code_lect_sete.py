521553c2fa43f3fed791e8b955f460da454573bd
dharmik097 <dvs97.cs@gmail.com>
2025-04-27 21:53:18 +0100

    new file

--- Python-practice-level-1/code_lect_sete.py
+++ Python-practice-level-1/code_lect_sete.py
@@ -1,5 +1,8 @@
+#file i/o in Python
 f = open("README.md", "r") #open the file in read mode.
 data = f.read() #read the file.
 print(data) #print the file.
 print(type(data))
 f.close() #close the file.  
+
+
