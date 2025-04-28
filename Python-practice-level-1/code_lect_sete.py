7392479da511f5cce03ca06cb5656f8e8e4a1bc4
dharmik097 <dvs97.cs@gmail.com>
2025-04-28 01:37:13 +0100

    adds

--- Python-practice-level-1/code_lect_sete.py
+++ Python-practice-level-1/code_lect_sete.py
@@ -65,7 +65,11 @@ def search_word_in_file():
     word = input("Enter the word to search: ") #take input from user.
     with open("practice.txt", "r") as f:
        data = f.read()
-    if(data.find("word")) != -1:
-        print("word found")
-    else:
-        print("word not found")
+       if(data.find(word)) != -1:
+           print("word found")
+       else:
+           print("word not found")
+
+search_word_in_file() #call the function.        
+
+# WAF to find in which line of the file does the word "learning" occur first time. print-1f the word is not found in the file.
