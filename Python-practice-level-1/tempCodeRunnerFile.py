a5594ec98f37ae634fd755d622570313c1f4c8b4
dharmik097 <dvs97.cs@gmail.com>
2025-05-02 11:34:54 +0100

    guess the number

--- Python-practice-level-1/tempCodeRunnerFile.py
+++ Python-practice-level-1/tempCodeRunnerFile.py
@@ -1,14 +1,13 @@
 
-class Order:
-    def __init__(self, item, price): # constructor
-        self.item = item # instance variable
-        self.price = price # instance variable
-    
-    def __gt__(self, order2):
-        return self.price > order2.price
+import random
+target = random.randint(1, 100) # target number
 
-order1 = Order("Laptop", 50000) # object creation
-order2 = Order("Mobile", 30000) # object creation
-
-print(order1 > order2) # Output: True
-print(order2 > order1) # Output: False
+while True: 
+    userChoice = int(input("Guess the number between 1 and 100: ")) # user input
+    if(userChoice == target) : # check if user guess is less than target
+        print("Congratulations! You guessed the number.")
+        break
+    elif (userChoice < target): # check if user guess is less than target       
+        print("Your guess is too low. Try again.")
+    else: # user guess is greater than target       
+        print("Your guess is too high. Try again.")
